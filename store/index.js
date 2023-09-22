7adc0e28d60ee05bd230a5268cb2277be0380ae9
AlexKon060698 <aleksander.kononenko2016@yandex.ru>
2023-09-23 02:29:45 +0300

    id

--- store/index.js
+++ store/index.js
@@ -25,23 +25,28 @@ export const mutations = {
 export const actions = {
   async fetchUsers({commit}, requests) {
     commit('setUsers', []);
+
     let users;
-    let checkArray = [];
+
     try {
       users = await this.$axios.$get('https://jsonplaceholder.typicode.com/users');
-      users.filter(user => {
-        requests.forEach(req => {
-          if (user.name.toLowerCase().includes(req) || user.username.toLowerCase() === req) if (!checkArray.some(item => item.username === user.username)) checkArray.push(user)
-        });
+
+      users = users.filter(user => {
+        return requests.some(req => user.name.toLowerCase().startsWith(req))
+        ||
+        requests.some(req => user.username.toLowerCase() === req)
+        ||
+        requests.some(req => user.id === Number(req));
       });
     } catch(e) {
       commit('setError', e);
     } finally {
-      commit('setUsers', checkArray);
+      commit('setUsers', users);
     }
   },
   async fetchUser({commit}, id) {
     let user;
+
     try {
       user = await this.$axios.$get('https://jsonplaceholder.typicode.com/users/' + id);
     } catch(e) {
